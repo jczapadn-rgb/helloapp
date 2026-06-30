9465912f10d3d73cc1a279bcf2f5d2d6b6eeba01
black <black@example.com>
2026-06-30 17:35:52 +0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+echo 'using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        PrintHello();
+    }
+
+    static void PrintHello()
+    {
+        Console.WriteLine("hello");
+    }
+}' > Program.cs
