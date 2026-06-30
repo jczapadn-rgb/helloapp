670c58728d0f936eeeffbe958a3ca25d3e195a3f
black <black@example.com>
2026-06-30 18:11:51 +0500

    Call greetWhite and greetBlack functions

--- Program.cs
+++ Program.cs
@@ -5,11 +5,18 @@ class Program
     static void Main(string[] args)
     {
         PrintHello();
+        greetWhite();
+        greetBlack();
     }
 
     static void PrintHello()
     {
         Console.WriteLine("hello everyone");
+    }
+
+    static void greetWhite()
+    {
+        Console.WriteLine("hello white");
     }
 
     static void greetBlack()
